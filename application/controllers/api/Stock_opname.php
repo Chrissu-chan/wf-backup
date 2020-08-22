c7e37d414fa92a38529cfd7f4705125bb50bb62e
Ahmad Afifudin <avivuv@gmail.com>
2020-08-22 10:34:41 +0700

    WF-73: Penyesuaian Fitur Stock Opname

--- application/controllers/api/Stock_opname.php
+++ application/controllers/api/Stock_opname.php
@@ -1,9 +1,9 @@
 <?php
-    defined('BASEPATH') OR exit('No direct script access allowed');
+	defined('BASEPATH') OR exit('No direct script access allowed');
 
     class Stock_opname extends BaseController {
 
-        public function __construct() {
+		public function __construct() {
             parent::__construct();
             $this->load->model('users_m');
             $this->load->model('barang_stok_m');
@@ -51,36 +51,46 @@
         }
 
         public function store() {
-            $post = $this->input->post();
+        	$post = $this->input->post();
             $stock_opname = $this->stock_opname_m->scope('active')->where('id_gudang', $post['id_gudang'])->first();
             if ($stock_opname) {
-                    $stock_opname_detail = $this->stock_opname_detail_m->view('stock_opname_detail')
-                        ->where('id_stock_opname', $stock_opname->id)
-                        ->where('barcode', $post['barcode'])
-                        ->order_by('id', 'desc')
-                        ->first();
-                    if ($stock_opname_detail) {
-                        $this->transaction->start();
-                        $record = array(
-                            'id_stock_opname' => $stock_opname->id,
-                            'id_obat' => $stock_opname_detail->id_obat,
-                            'selisih' => $post['jumlah'] - $stock_opname_detail->stok_awal,
-	                        'hna' => (isset($post['hna']) ? $post['hna'] : NULL),
-	                        'expired' => (isset($post['expired']) ? $post['expired'] : NULL),
-                            'so_by' => $this->auth->username
-                        );
-	                    $this->stock_opname_detail_m->update($stock_opname_detail->id, $record);
-                        if ($this->transaction->complete()) {
-                            $response = array(
-                                'success' => true,
-                                'message' => $this->localization->lang('stock_opname_berhasil_disimpan')
-                            );
-                        } else {
-                            $response = array(
-                                'success' => false,
-                                'message' => $this->localization->lang('stock_opname_gagal_disimpan')
-                            );
-                        }
+                $stock_opname_detail = $this->stock_opname_detail_m->view('stock_opname_detail')
+                    ->where('id_stock_opname', $stock_opname->id)
+                    ->where('barcode', $post['barcode'])
+                    ->order_by('id', 'desc')
+                    ->first();
+                if ($stock_opname_detail) {
+                	$barang_stok = $this->barang_stok_m->where('id_gudang', $stock_opname->id_gudang)
+		                ->where('id_barang', $stock_opname_detail->id_barang)
+		                ->first();
+                	if ($barang_stok) {
+		                $this->transaction->start();
+		                $record = array(
+			                'id_stock_opname' => $stock_opname->id,
+			                'id_obat' => $stock_opname_detail->id_obat,
+			                'selisih' => $post['jumlah'] - $barang_stok->jumlah,
+			                'hna' => (isset($post['hna']) ? $post['hna'] : NULL),
+			                'expired' => (isset($post['expired']) ? $post['expired'] : NULL),
+			                'so_by' => $this->auth->username
+		                );
+		                $this->stock_opname_detail_m->update($stock_opname_detail->id, $record);
+		                if ($this->transaction->complete()) {
+			                $response = array(
+				                'success' => true,
+				                'message' => $this->localization->lang('stock_opname_berhasil_disimpan')
+			                );
+		                } else {
+			                $response = array(
+				                'success' => false,
+				                'message' => $this->localization->lang('stock_opname_gagal_disimpan')
+			                );
+		                }
+	                } else {
+		                $response = array(
+			                'success' => false,
+			                'message' => $this->localization->lang('barang_tidak_sesuai')
+		                );
+	                }
                 } else {
                     $response = array(
                         'success' => false,
